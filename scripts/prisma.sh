4e9eabec6e0d2c76c969b905a1e1c7962acb9087
Jesús Quirós <jquirossoto@gmail.com>
2021-10-19 16:00:06 -0600

    test script

--- scripts/prisma.sh
+++ scripts/prisma.sh
@@ -4,6 +4,7 @@ if [[ -z "${DATABASE_HOST}" ]]; then
     >&2 echo "DATABASE_HOST does not exists"
     exit 1
 else
+    >&2 echo "DATABASE_HOST does exists"
     MY_SCRIPT_VARIABLE="${DATABASE_HOST}"
     >&2 echo $MY_SCRIPT_VARIABLE
 fi
