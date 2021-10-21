fc0546e04d9868b451b0491cabc3e744da4c3ca6
Jesús Quirós <jquirossoto@gmail.com>
2021-10-21 12:11:57 -0600

    improves prisma script

--- scripts/prisma.sh
+++ scripts/prisma.sh
@@ -1,39 +1,63 @@
 #!/bin/bash
 
-if [[ -z "${DATABASE_HOST}" ]]; then
-    >&2 echo "DATABASE_HOST env variable does not exists"
+urlencode() {
+    # taken from https://github.com/SixArm/urlencode.sh
+    # urlencode <string>
+
+    old_lang=$LANG
+    LANG=C
+    
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LANG=$old_lang
+    LC_COLLATE=$old_lc_collate
+}
+
+if [[ -z "${DB_HOST}" ]]; then
+    >&2 echo "DB_HOST env variable does not exists"
     exit 1
 else
-    HOST="${DATABASE_HOST}"
+    HOST="${DB_HOST}"
 fi
 
-if [[ -z "${DATABASE_PORT}" ]]; then
-    >&2 echo "DATABASE_PORT env variable does not exists"
+if [[ -z "${DB_PORT}" ]]; then
+    >&2 echo "DB_PORT env variable does not exists"
     exit 1
 else
-    PORT="${DATABASE_PORT}"
+    PORT="${DB_PORT}"
 fi
 
-if [[ -z "${DATABASE_NAME}" ]]; then
-    >&2 echo "DATABASE_NAME env variable does not exists"
+if [[ -z "${DB_NAME}" ]]; then
+    >&2 echo "DB_NAME env variable does not exists"
     exit 1
 else
-    NAME="${DATABASE_NAME}"
+    NAME="${DB_NAME}"
 fi
 
-if [[ -z "${DATABASE_USERNAME}" ]]; then
-    >&2 echo "DATABASE_USERNAME env variable does not exists"
+if [[ -z "${DB_USERNAME}" ]]; then
+    >&2 echo "DB_USERNAME env variable does not exists"
     exit 1
 else
-    USERNAME="${DATABASE_USERNAME}"
+    USERNAME="${DB_USERNAME}"
 fi
 
-if [[ -z "${DATABASE_PASSWORD}" ]]; then
-    >&2 echo "DATABASE_PASSWORD env variable does not exists"
+if [[ -z "${DB_PASSWORD}" ]]; then
+    >&2 echo "DB_PASSWORD env variable does not exists"
     exit 1
 else
-    PASSWORD="${DATABASE_PASSWORD}"
+    PASSWORD=$(urlencode ${DB_PASSWORD})
 fi
+
+echo $PASSWORD
 
 echo "DATABASE_URL=postgresql://$USERNAME:$PASSWORD@$HOST:$PORT/$NAME" > ./prisma/.env
->&2 echo "DATABASE_URL=postgresql://$USERNAME:$PASSWORD@$HOST:$PORT/$NAME"
