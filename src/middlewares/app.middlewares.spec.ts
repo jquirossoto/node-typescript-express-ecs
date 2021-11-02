569433ffaae29b5585586a4229b8f1af341b355c
Jesús Quirós <jquirossoto@gmail.com>
2021-11-01 19:30:30 -0600

    adds app.middlewares tests

---
+++ src/middlewares/app.middlewares.spec.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from 'express';
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import Ajv, { JSONSchemaType } from "ajv";
+
+import { buildErrorResponse } from './../utils/api.utils';
+import { authorize, validateSchema } from './app.middlewares';
+import Category from './../models/category.model';
+
+describe('App Middlewares', () => {
+
+    describe('authorize()', () => {
+
+        it('Should authorize the request', () => {
+            const req: Request = getMockReq({
+                headers: {
+                    'x-api-key': '123'
+                }
+            });
+            const { res, next } = getMockRes();
+            authorize(req, res, next);
+            expect(next).toHaveBeenCalledTimes(1);
+        });
+
+        it('Should return 403 error', () => {
+            const req: Request = getMockReq();
+            const { res, next } = getMockRes();
+            authorize(req, res, next);
+            expect(res.status).toHaveBeenCalledWith(403);
+            expect(res.json).toHaveBeenCalledWith(buildErrorResponse(['UNAUTHORIZED']))
+            expect(next).toHaveBeenCalledTimes(0);
+        });
+
+    });
+
+    describe('validateSchema()', () => {
+
+        it('Should validate the schema', () => {
+            const newCategory: Category = {
+                name: 'My category'
+            };
+            const req: Request = getMockReq({
+                body: newCategory
+            });
+            const { res, next } = getMockRes();
+            const schema = {
+                type: "object",
+                properties: {
+                    name: {
+                        type: "string"
+                    }
+                },
+                required: ["name"],
+                additionalProperties: false
+            }
+            const validateSchemaMiddleware = validateSchema(schema);
+            validateSchemaMiddleware(req, res, next);
+            expect(next).toHaveBeenCalledTimes(1);
+        });
+
+        it('Should return 403 error', () => {
+            const req: Request = getMockReq({
+                body: {
+                    name1: 'My category'
+                }
+            });
+            const { res, next } = getMockRes();
+            const schema = {
+                type: "object",
+                properties: {
+                    name: {
+                        type: "string"
+                    }
+                },
+                required: ["name"],
+                additionalProperties: false
+            }
+            const validateSchemaMiddleware = validateSchema(schema);
+            validateSchemaMiddleware(req, res, next);
+            expect(res.status).toHaveBeenCalledWith(422);
+            expect(next).toHaveBeenCalledTimes(0);
+        });
+
+    });
+
+});
